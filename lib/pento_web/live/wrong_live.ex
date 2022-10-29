bd02824ff7a06c5c25f810e8a88298704aae0db7
Christopher Eyre <chriseyre2000@gmail.com>
2022-10-29 13:04:23 +0100

    Added auth. End of chapter 2

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -1,10 +1,16 @@
 defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
 
-  def mount(_params, _session, socket) do
+  def mount(_params, session, socket) do
     {
       :ok,
-      assign(socket, score: 0, message: "Guess a number.", answer: Enum.random(1..10))
+      assign(socket,
+        score: 0,
+        message: "Guess a number.",
+        answer: Enum.random(1..10),
+        user: Pento.Accounts.get_user_by_session_token(session["user_token"]),
+        session_id: session["live_socket_id"]
+      )
     }
   end
 
@@ -19,19 +25,24 @@ defmodule PentoWeb.WrongLive do
         <a href="#" phx-click="guess" phx-value-number="<%= n %>"><%= n %>  </a>
       <% end %>
     </h2>
+    <pre>
+      <%= @user.email %>
+      <%= @session_id %>
+    </pre>
     """
   end
 
   def handle_event("guess", %{"number" => guess} = data, socket) do
     IO.inspect(socket.assigns, label: :assigns)
 
-    message = "Your guess: #{guess}. Wrong. Guess again. "
+    # message = "Your guess: #{guess}. Wrong. Guess again. "
 
     {score, message, answer} =
       if guess == "#{socket.assigns.answer}" do
         {socket.assigns.score + 1, "Well done. Try another.", Enum.random(1..10)}
       else
-        {socket.assigns.score - 1, "Your guess: #{guess}. Wrong. Guess again. ", socket.assigns.answer}
+        {socket.assigns.score - 1, "Your guess: #{guess}. Wrong. Guess again. ",
+         socket.assigns.answer}
       end
 
     {
